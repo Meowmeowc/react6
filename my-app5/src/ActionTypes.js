8e1da98146b36ac956df070c2404e9a186ba067f
Meowmeowc <lalachka26@mail.ru>
2023-03-22 21:12:51 +0500

    first commit

---
+++ my-app5/src/ActionTypes.js
@@ -0,0 +1,52 @@
+// // ТИПЫ ЭКШЕНОВ
+// export const ADD_TODO = 'ADD_TODO'
+// export const TOOGLE_TODO = 'TOOGLE_TODO'
+// export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
+
+
+// // Другие константы
+// export const visibilityFilters = {
+// 	SHOW_ALL: 'SHOW_ALL',
+// 	SHOW_COMPLETED: 'SHOW_COMPLETED',
+// 	SHOW_ACTIVE: 'SHOW_ACTIVE'
+// }
+
+
+// // ГЕНЕРАТОР ЭКШЕНОВ
+
+// export function AddTodo(text) {
+// 	return { type: ADD_TODO, text }
+// }
+
+// export function ToogleTodo(index) {
+// 	return { type: TOOGLE_TODO, index }
+// }
+// export function SET_VISIBILITY_FILTER(filter) {
+// 	return { type: SET_VISIBILITY_FILTER, filter }
+// }
+
+
+
+// // import { ADD_TODO, REMOVE_TODO } '../папка'
+
+
+// // // ACTION
+// // const ADD_TODO = 'ADD'
+// // {
+// // 	type: ADD_TODO,
+// // 		text: 'Build my first Redux app'
+// // }
+// // {
+// // 	type: TOOGLE_TODO,
+// // 		index: 5
+// // }
+
+// // {
+// // 	type: SET_VISIBILITY_FILTER,
+// // 		filter: SHOW_COMPLETED
+// // }
+
+
+// // //  ACTION CREATER
+
+// // boundAddTodo(text)
